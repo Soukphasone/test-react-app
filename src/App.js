61e2262a5388d25c89c92cea1726da7308c7306c
soukphasone <soukphasone2001@gmail.com>
2024-03-24 13:49:38 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@
 import React from 'react';
 
 function App() {
+  // Define the Toaster object
+
+
   const sendDataToFlutter = () => {
     // Data to send to Flutter WebView
-    const data = {
-      message: 'Hello from React!',
-      someValue: 42,
-    };
+    // const data = {
+    //   message: 'Hello from React!',
+    //   someValue: 42,
+    // };
 
-    // Send data to Flutter WebView
-    window.Toaster.postMessage(data, '*');
+    // // Send data to Flutter WebView
+    // window.Toaster.postMessage(data, '*');
+    var Toaster = {
+      postMessage: function(message) {
+        // Implementation of postMessage method
+        console.log(message);
+      }
+    };
+    
+    // Now you can use the Toaster object
+    Toaster.postMessage("Hello, world!");
   };
 
   return (
